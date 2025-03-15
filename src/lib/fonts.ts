ffa6f6555c835e7af5b1715d8170f1a07f4ae465
Stoble <stoble@mail.ru>
2025-03-15 15:46:39 +0300

    feat: catalog grid buttons

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,27 +1,3 @@
-/* @font-face {
-  font-family: "AkiraExpanded";
-  src: url("/fonts/Akira-Expanded.otf") format("opentype");
-  font-weight: normal;
-  font-style: normal;
-  font-display: swap;
-}
-
-@font-face {
-  font-family: "Lato";
-  src: url("/fonts/Lato-Regular.ttf") format("truetype");
-  font-weight: 400;
-  font-style: normal;
-  font-display: swap;
-}
-
-@font-face {
-  font-family: "Lato";
-  src: url("/fonts/Lato-Bold.ttf") format("truetype");
-  font-weight: 700;
-  font-style: normal;
-  font-display: swap;
-} */
-
 import { Lato } from "next/font/google";
 import localFont from "next/font/local";
 
