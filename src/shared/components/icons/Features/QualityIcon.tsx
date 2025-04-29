9d64eb047234cd66105807167c5d8665ae772adb
Stoble <stoble@mail.ru>
2025-04-29 19:53:04 +0300

    fix: build errors

--- src/shared/components/icons/Features/QualityIcon.tsx
+++ src/shared/components/icons/Features/QualityIcon.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/shared/lib/utils";
+import { cn } from "@/shared/utils/cn";
 import { SVGProps } from "react";
 
 const QualityIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
